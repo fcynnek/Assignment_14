8145e47c81038519c96f2f80e88b85cb2e5b096d
fcynnek <kchengfung@gmail.com>
2023-06-11 19:56:39 -0400

    Repository

--- src/main/java/com/fcynnek/Assignment_14/repository/UserRepository.java
+++ src/main/java/com/fcynnek/Assignment_14/repository/UserRepository.java
@@ -1,8 +1,26 @@
 package com.fcynnek.Assignment_14.repository;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+
+import com.fcynnek.Assignment_14.domain.User;
 
 public class UserRepository {
 
+//	private List<User> users = new ArrayList<>();
+	Integer userId = 0;
+	HashMap<Integer, String> users = new HashMap<Integer, String>();
 	
+	public int saveUser (Integer userId, String user) {
+		if (users.containsValue(user)) {
+//			return saveUser(userId, user);
+			return user.indexOf(userId);
+		} else {
+			User newUser = new User(userId, user);
+			newUser.setUserId(userId++);
+			newUser.setUsername(user);
+			return users.add(newUser);
+		}
+	}
 }

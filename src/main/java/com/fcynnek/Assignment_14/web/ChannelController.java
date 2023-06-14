b1e180ff2f3ab7305f99bc7039d3844de537c33a
fcynnek <kchengfung@gmail.com>
2023-06-14 18:41:19 -0400

    Working on controllers

---
+++ src/main/java/com/fcynnek/Assignment_14/web/ChannelController.java
@@ -0,0 +1,9 @@
+package com.fcynnek.Assignment_14.web;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChannelController {
+
+	
+}
--- src/main/java/com/fcynnek/Assignment_14/web/WelcomeController.java
+++ src/main/java/com/fcynnek/Assignment_14/web/WelcomeController.java
@@ -1,23 +1,29 @@
 package com.fcynnek.Assignment_14.web;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.fcynnek.Assignment_14.domain.User;
+import com.fcynnek.Assignment_14.service.ChannelService;
 import com.fcynnek.Assignment_14.service.UserService;
 
 @Controller
 public class WelcomeController {
 
+	@Autowired
+	private ChannelService channelService;
+	
+	@Autowired
 	private UserService userService;
-	private User userDomain;
 	
 	@GetMapping("/welcome")
-	public String getUsername (ModelMap model) {
-		User user = new User(userDomain.getUserId(), userDomain.getUsername());
-//		userService.createUser(user);
+	public String getUsername (ModelMap model, String username) {
+		User user = userService.createUser(username);
 		model.put("user", user);
-		return "channels";
+		return "welcome";
 	}
 }

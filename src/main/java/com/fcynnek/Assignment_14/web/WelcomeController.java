94be017d5189a0d23055f4fa8423b4175cd3719c
fcynnek <kchengfung@gmail.com>
2023-06-15 20:33:19 -0400

    Working landing page

--- src/main/java/com/fcynnek/Assignment_14/web/WelcomeController.java
+++ src/main/java/com/fcynnek/Assignment_14/web/WelcomeController.java
@@ -26,4 +26,6 @@ public class WelcomeController {
 		model.put("user", user);
 		return "welcome";
 	}
+	
+	
 }
--- src/main/resources/static/js/welcome.js
+++ src/main/resources/static/js/welcome.js
@@ -1,2 +1,9 @@
 var messageIcon = document.querySelector('.fa-message-smile')
 var submitIcon = document.querySelector('.fa-paper-plane')
+var messageInput = document.querySelector('.message-input')
+var user = sessionStorage.getItem('user')
+
+if (user === null) {
+    alert('Please input a username')
+} 
+

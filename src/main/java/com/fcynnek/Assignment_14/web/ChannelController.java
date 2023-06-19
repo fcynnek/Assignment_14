b25eaf9e8a7bbca4eafc46f6d9596e16e8b696ba
fcynnek <kchengfung@gmail.com>
2023-06-18 20:43:03 -0400

    Channel endpoint

--- src/main/java/com/fcynnek/Assignment_14/web/ChannelController.java
+++ src/main/java/com/fcynnek/Assignment_14/web/ChannelController.java
@@ -1,9 +1,25 @@
 package com.fcynnek.Assignment_14.web;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.fcynnek.Assignment_14.domain.Channel;
+import com.fcynnek.Assignment_14.service.ChannelService;
 
 @Controller
 public class ChannelController {
 
+	@Autowired
+	private ChannelService channelService;
 	
+	@GetMapping("/channels")
+	public String getChannels (ModelMap model) {
+		List<Channel> channels = channelService.findAll();
+		model.put("channels", channels);
+		return "channels";
+	}
 }
---
+++ src/main/resources/static/js/channels.js
@@ -0,0 +1,3 @@
+document.getElementById('createChannel').addEventListener('click', function() {
+    window.location.href = '/channels'
+})

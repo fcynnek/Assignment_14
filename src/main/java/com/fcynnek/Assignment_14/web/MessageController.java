99aa8210e8b0c0e83adead3da55bef086cc9eb76
fcynnek <kchengfung@gmail.com>
2023-07-06 20:18:29 -0400

    no update

--- src/main/java/com/fcynnek/Assignment_14/web/MessageController.java
+++ src/main/java/com/fcynnek/Assignment_14/web/MessageController.java
@@ -1,6 +1,9 @@
 package com.fcynnek.Assignment_14.web;
 
+import java.util.Collection;
 import java.util.List;
+
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,10 +13,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fcynnek.Assignment_14.domain.Channel;
 import com.fcynnek.Assignment_14.domain.Message;
+import com.fcynnek.Assignment_14.domain.User;
 import com.fcynnek.Assignment_14.service.ChannelService;
 import com.fcynnek.Assignment_14.service.MessageService;
 
@@ -32,10 +38,29 @@ public class MessageController {
     }
 
     @GetMapping("/channels/{channelId}")
-    public String getChannelMessages(@PathVariable Integer channelId, ModelMap model) {
-        List<Message> messages = messageService.getMessages(channelId);
-        model.addAttribute("messages", messages);
-        model.addAttribute("channelId", channelId);
+    public String getChannelMessages(@PathVariable Integer channelId, ModelMap model, HttpSession session, @RequestParam("username") String username,
+    		@RequestParam(value="message") String message) {
+    	User user = new User();
+		user.setUsername(username);
+		session.setAttribute("user", user);
+		session.setAttribute("username", username);
+
+		Channel channel = channelService.findChannelById(channelId);
+		channel.addUser(user);
+		session.setAttribute("channel", channel);
+
+		if (message != null && !message.isEmpty()) {
+			Message chatMessage = new Message();
+			chatMessage.setMessage(message);
+			chatMessage.setUser(user);
+			chatMessage.setUsername(username);
+			chatMessage.setChannelId(channel.getChannelId());
+			channel.addMessageOfTypeString(message);
+		}
+		
+		model.addAttribute("user", user);
+		model.addAttribute("channel", channel);
+		model.addAttribute("messages", channel.getMessages());
         return "channels";
     }
 

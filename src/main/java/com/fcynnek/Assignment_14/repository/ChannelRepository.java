2c0665d04217be8d861f0c9a89b672e9aa8e72ee
fcynnek <kchengfung@gmail.com>
2023-07-14 05:47:44 -0400

    Linking to SQL DB

--- src/main/java/com/fcynnek/Assignment_14/repository/ChannelRepository.java
+++ src/main/java/com/fcynnek/Assignment_14/repository/ChannelRepository.java
@@ -1,40 +1,12 @@
 package com.fcynnek.Assignment_14.repository;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
-import org.springframework.stereotype.Component;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.fcynnek.Assignment_14.domain.Channel;
-import com.fcynnek.Assignment_14.domain.User;
 
-@Component
-public class ChannelRepository {
+@Repository
+public interface ChannelRepository extends JpaRepository<Channel, Integer>{
 
-	private List<Channel> channels = new ArrayList<>();
-	private List<User> users = new ArrayList<>();
 	
-//	public HashMap<Integer, Channel> findAll() {
-//		return channels;
-//	}
-	public List<Channel> findAll() {
-		return channels;
-	}
-	
-	public Channel saveChannel(Integer channelId, String channelName) {
-		Channel channel = new Channel();
-		channel.setChannelId(channelId);
-		channel.setChannelName(channelName);
-		channels.add(channelId, channel);
-		return channel;
-	}
-	
-	public Channel findChannelById (Integer channelId) {
-		return channels.get(channelId);
-	}
-
-//	public Channel save(Channel channel) {
-//		return ((Object) channels).put(channel);
-//	}
 }

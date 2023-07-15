bf5620b67036333179be2ed0d5aa6f0b4c4a7251
fcynnek <kchengfung@gmail.com>
2023-07-15 17:06:03 -0400

    Letting Spring handle the channel ID

--- src/main/resources/static/js/channels.js
+++ src/main/resources/static/js/channels.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
   var addChannelButton = document.getElementById('addChannelButton');
   var channelsContainer = document.getElementById('channelsContainer');
-  var channelIdCounter = sessionStorage.getItem('channelIdCounter') || 1;
 
   addChannelButton.addEventListener('click', function () {
     var channelName = prompt("Enter channel name:");
@@ -11,16 +10,29 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   function createChannel(channelName) {
-    var channelId = channelIdCounter++;
-    var newChannelElement = document.createElement('div');
-    var newChannelLink = document.createElement('a');
-    newChannelLink.textContent = channelName;
-    newChannelLink.href = '/channels/' + channelId;
-    newChannelLink.addEventListener('click', function () {
-      sessionStorage.setItem('channelId', channelId);
-    });
-    newChannelElement.appendChild(newChannelLink);
-    channelsContainer.prepend(newChannelElement);
+    // Send a request to the backend to create a new channel
+    fetch('/channels', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ channelName: channelName })
+    })
+      .then(response => response.json())
+      .then(channel => {
+        // After successfully creating the channel, update the UI
+        var channelId = channel.channelId;
+        var newChannelElement = document.createElement('div');
+        var newChannelLink = document.createElement('a');
+        newChannelLink.textContent = channelName;
+        newChannelLink.href = '/channels/' + channelId;
+        newChannelLink.addEventListener('click', function () {
+          sessionStorage.setItem('channelId', channelId);
+        });
+        newChannelElement.appendChild(newChannelLink);
+        channelsContainer.prepend(newChannelElement);
+      })
+      .catch(error => console.error('Error creating channel:', error));
   }
 
 });

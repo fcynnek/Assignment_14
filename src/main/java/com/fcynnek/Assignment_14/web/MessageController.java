d8280d25e7b22c8f789846dece6d334996a3a1b7
fcynnek <kchengfung@gmail.com>
2023-07-24 19:43:45 -0400

    WIP

--- src/main/java/com/fcynnek/Assignment_14/web/MessageController.java
+++ src/main/java/com/fcynnek/Assignment_14/web/MessageController.java
@@ -56,15 +56,15 @@ public class MessageController {
 	
 	@PostMapping("/channel/{channelId}")
 	@ResponseBody
-	public Message sendMessage(@PathVariable Integer channelId, @RequestBody Message message) {
+	public Message sendMessage(@PathVariable Integer channelId, @RequestBody String message) {
 //	public Message sendMessage(@PathVariable Integer channelId,
-//			@RequestParam("message") Message message, @RequestParam("user") Message user) {
+//			@RequestParam("message") Message message, @RequestParam("user") User user) {
 		Channel channel = channelService.findChannelById(channelId);
 		Message chat = new Message();
 		
 		chat.addMessage(message);
 		chat.setChannel(channel);
-		chat.setUser(message.getUser());
+		chat.setUser(user);
 		channel.getMessages().add(chat);
 		channelService.saveChannel(channel);
 		System.out.println(message);
